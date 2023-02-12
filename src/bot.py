53475b9051c9e7342ff2479c4677ca1bbe11465b
ShahafPinto <shahafchen7@gmail.com>
2023-02-12 18:19:41 +0200

    clean code

--- src/bot.py
+++ src/bot.py
@@ -1,52 +1,39 @@
 import logging
-from pprint import pprint
 from pymongo import MongoClient
-
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, \
     Filters, Updater, CallbackQueryHandler
 import bot_settings
 from src.class_application import Application
 
-logging.basicConfig(
-    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
-    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
 updater = Updater(token=bot_settings.BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
+# logging:
+logging.basicConfig(
+    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#DB:
 client = MongoClient()
 db = client.get_database('job_application')
 job_collection = db.get_collection('job_collection')
-
 
 keyboard = [
     [InlineKeyboardButton("הכנס משרה", callback_data='add')],
     [InlineKeyboardButton("הצג את כל המשרות", callback_data='display_all')],
     [InlineKeyboardButton("עדכן משרה", callback_data='update')],
 ]
-
 update_keyboard = []
-# def create_update_keyboard():
-#     global update_keyboard
-#     for key,value in all_apps.items():
-#         update_keyboard.append([InlineKeyboardButton(str(value), callback_data=f'{key}')])
-
 reply_markup = InlineKeyboardMarkup(keyboard)
 reply_update_markup = InlineKeyboardMarkup(update_keyboard)
+
 
 counter = 0
 new_app = {}
 all_apps = {}
 
-# def help_command(update: Update, context: CallbackContext):
-#     chat_id = update.effective_chat.id
-#     user = update.message.from_user
-#     context.bot.send_message(chat_id=chat_id, text=f"{user.first_name}, Use /start to restart the game.")
-#     # update.message.reply_text("Use /start to restart the game.")
-#
 
 def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
@@ -60,13 +47,10 @@ def start(update: Update, context: CallbackContext):
 
 
 def button(update: Update, context: CallbackContext) -> None:
-    # print('here')
     query = update.callback_query
     query.answer()
     global all_apps
-    # print(query.data)
     chat_id = update.effective_chat.id
-    # guess_status = update.message.text
     logger.info(f"= Got on chat #{chat_id}: {query.data!r}")
     if query.data == "add":
         context.bot.send_message(chat_id=chat_id, text='מה שם החברה?')
@@ -89,11 +73,9 @@ def button(update: Update, context: CallbackContext) -> None:
     else:
         job_collection.find_one_and_update({"chat_id": str(chat_id), "company": query.data}, {"$set": {"status": "Rejected"}})
 
-        # app_to_update = all_apps[query.data]
-        # app_to_update.set_status('Rejected')
-
         context.bot.send_message(chat_id=chat_id, text='המשרה עודכנה!')
         context.bot.send_message(chat_id=chat_id, text='מה תרצה לעשות?', reply_markup=reply_markup)
+
 
 def add_new_app(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
@@ -110,8 +92,6 @@ def add_new_app(update: Update, context: CallbackContext):
         new_app['stack'] = update.message.text
         app = Application(new_app['company'], new_app['title'], new_app['stack'])
         all_apps[app.get_company()] = app
-        print(app)
-        # print(app.company,app.title,app.status,app.stack)
         job_collection.insert_one({
             "chat_id": str(chat_id),
             "company": app.get_company(),
@@ -125,7 +105,6 @@ def add_new_app(update: Update, context: CallbackContext):
         new_app = {}
         context.bot.send_message(chat_id=chat_id, text='המשרה הוכנסה !')
         start(update, context)
-    # pprint(all_apps)
 
 
 start_handler = CommandHandler('start', start)
