1be03a4c61e1c18c4843457d8fbe3063a0188167
shaharmes <shaharmes33@gmail.com>
2023-02-13 12:33:36 +0200

    openAI API added

---
+++ src/openAPI.py
@@ -0,0 +1,22 @@
+import openai
+
+
+def get_company_info(name):
+    openai.api_key = "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"
+    location = 'Israel'
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f'Please tell me about the company named {name} located in {location}',
+
+        temperature=0.5,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0)
+
+    text = ' ' + response.choices[0]["text"]
+    lines = text.split('.')
+    pretty_text = '\n'.join(lines).strip()
+    print(f'>>> Information about {name}:\n', pretty_text)
+
